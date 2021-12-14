18c2ec5a68a7ecfa60d6f768cbb520175c39c6a4
Paper Girl <jamie.rose.528@gmail.com>
2021-12-13 21:11:35 -0800

    fixed employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,33 +1,34 @@
 const Employee = require("../lib/Employee");
 
-test("all properties of Employee", () => {
-  test("name", () => {
+describe("all properties of Employee", () => {
+  it("name", () => {
     const employee = new Employee("Mike", "332", "mike@mike.com");
     expect(employee.name).toBe("Mike");
   });
-  test("id", () => {
+  it("id", () => {
     const employee = new Employee("Mike", "332", "mike@mike.com");
     expect(employee.id).toBe("332");
   });
-  test("email", () => {
+  it("email", () => {
     const employee = new Employee("Mike", "332", "mike@mike.com");
     expect(employee.email).toBe("mike@mike.com");
   });
 });
-test("all methods of Employee", () => {
-  test("getName", () => {
+
+describe("all methods of Employee", () => {
+  it("getName", () => {
     const employee = new Employee("Jenn", "888", "jenn@jenn.com");
     expect(employee.getName()).toBe("Jenn");
   });
-  test("getId", () => {
+  it("getId", () => {
     const employee = new Employee("Jenn", "888", "jenn@jenn.com");
     expect(employee.getId()).toBe("888");
   });
-  test("getEmail", () => {
+  it("getEmail", () => {
     const employee = new Employee("Jenn", "888", "jenn@jenn.com");
     expect(employee.getEmail()).toBe("jenn@jenn.com");
   });
-  test("getRole", () => {
+  it("getRole", () => {
     const employee = new Employee("Jenn", "888", "jenn@jenn.com");
     expect(employee.getRole()).toBe("Employee");
   });
